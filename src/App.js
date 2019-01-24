5457e808981d3bf58070705fcf31288faa0f4d8a
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-01-24 12:51:51 -0700

    Got card to populate index[0] data

--- src/App.js
+++ src/App.js
@@ -8,15 +8,13 @@ class App extends Component {
   constructor () {
     super()
       this.state = {
-        flashcards: [],
-        flashcard: false,
+        flashcards: data,
         id: 0,
         name: "",
         description: "",
         example: "",
         tags: [],
         link: "",
-        data: data
       }
   }
 
@@ -34,6 +32,15 @@ class App extends Component {
   flashCard = (e) => {
     e.preventDefault()
     console.log("Botton was clicked")
+    console.log(this.state.flashcards.methods[0].name)
+    this.setState({
+      name: this.state.flashcards.methods[0].name,
+      description: this.state.flashcards.methods[0].description,
+      example: this.state.flashcards.methods[0].example,
+      // tags: flashcards[0].tags,
+      link: this.state.flashcards.methods[0].link,
+    })
+
   }
 
   render() {
@@ -45,16 +52,29 @@ class App extends Component {
           <div className="col-8 text-center">
             <h1 className="pb-2">JS FLashCards</h1>
             <p className="pb-2">Learning JS just got easy!</p>
+
+            <Card
+              flashcards={this.state.flashcards}
+              id={this.state.id}
+              name={this.state.name}
+              description={this.state.description}
+              example={this.state.example}
+              tags={this.state.tags}
+              link={this.state.link}
+            />
+
             <button 
-            className="btn btn-danger btn-lg" 
-            onClick={this.flashCard}
+              className="btn btn-danger btn-lg" 
+              onClick={this.flashCard}
             >
             Click to Study
             </button>
           </div>
         </div>
         <div className="row justify-content-center">
-          {this.state.flashcard ? <Card
+
+          
+          {/* {this.state.flashcard ? <Card
             id={this.state.id}
             name={this.state.name}
             description={this.state.description}
@@ -62,7 +82,7 @@ class App extends Component {
             tags={this.state.tags}
             link={this.state.link}
           />
-            : <div></div>}
+            : <div></div>} */}
             
         </div>
       </div>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import '../App.css';
-import Button from './Button.js'
+// import Button from './Button.js'
 
 const Card = (props) => {
   return (
     <div className="card col-sm-4 mx-2 my-2">
       <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">{props.quote}</p>
-        {props.tags.map((tag, idx) => {
-          return <Button 
-            key={idx}
-            tag={tag}/>
-        })}
+        <h3 className="card-title">Name: {props.name}</h3>
+        <h5 className="card-text">Description: {props.description}</h5>
+        <h5 className="card-text">Example: {props.example}</h5>
+        <h5 className="card-text">Tags: {props.tags}</h5>
+        <h5 className="card-text">Link: {props.link}</h5>
       </div>
     </div>
   )
